e9120badf2e529096eb8cb7dabe01974a1201a48
Alessandro Marega <ale_marega@hotmail.it>
2021-02-26 15:43:19 +0100

    aggiunte variabili dei prezzi

--- js/script.js
+++ js/script.js
@@ -4,4 +4,11 @@ var chilometri = parseInt(prompt("Inserisci i chilometri che intendi percorrere"
 // richiesta eta del passeggero
 var eta = parseInt(prompt("Inserisci la tua eta\'"));
 
-// prezzo totale del viaggio 
+// prezzi
+var prezzoNonScontato = chilometri * 0.21;
+
+var prezzoMinorenni = prezzoNonScontato - ((prezzoNonScontato / 100) * 20);
+
+var prezzoOver65 = prezzoNonScontato - ((prezzoNonScontato / 100) * 40);
+
+// prezzo totale del viaggio
