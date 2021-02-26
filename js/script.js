3f91427f25db2cae7194ad90a1e6a2ac9ec823de
Alessandro Marega <ale_marega@hotmail.it>
2021-02-26 15:48:38 +0100

    aggiunto calcolo del prezzo totale di viaggio per le tre fasce d'età

--- js/script.js
+++ js/script.js
@@ -12,3 +12,10 @@ var prezzoMinorenni = prezzoNonScontato - ((prezzoNonScontato / 100) * 20);
 var prezzoOver65 = prezzoNonScontato - ((prezzoNonScontato / 100) * 40);
 
 // prezzo totale del viaggio
+if ( eta < 18 ) {
+  alert("Il prezzo del tuo biglietto e\' di " + prezzoMinorenni + "\u20AC");
+} else if ( eta >= 18 && eta < 65 ) {
+  alert("Il prezzo del tuo biglietto e\' di " + prezzoNonScontato + "\u20AC");
+} else {
+  alert("Il prezzo del tuo biglietto e\' di " + prezzoOver65 + "\u20AC");
+}
