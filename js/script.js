838b92f8e70f421983aa16f6805ff84f7a3711ac
Alessandro Marega <ale_marega@hotmail.it>
2021-02-26 15:21:06 +0100

    aggiunte richieste chilometraggio ed età

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
+// richiesta km da percorrere
+var chilometri = parseInt(prompt("Inserisci i chilometri che intendi percorrere"));
 
+// richiesta eta del passeggero
+var eta = parseInt(prompt("Inserisci la tua eta\'"));
+
+// prezzo totale del viaggio 
